6b7a01e7f4335971f22517481c84be2c2f29462e
matthras <matthew.y.p.mack@gmail.com>
2023-11-25 19:10:40 +1100

    added equality tests

--- Vectors/Anathema.Vectors.Tests/FloatComplex/comTests.cs
+++ Vectors/Anathema.Vectors.Tests/FloatComplex/comTests.cs
@@ -46,5 +46,54 @@ namespace Anathema.Vectors.Tests.Complex
             Assert.Equal(5, fromDcom.imaginary);
         }
 
+        [Fact]
+        public void equality()
+        {
+            float a = 1.0f;
+            float b = a / 3.0f;
+            float c = b * 3.0f;
+            float d = 0.999999999999f;
+
+            com x = new com(a, c);
+            com y = new com(c, a);
+            com z = new com(d, d);
+            com w = new com(1.1f, a);
+            com nullvec = null;
+
+            Assert.True(x.Equals(y));
+            Assert.False(x.Equals(nullvec));
+            Assert.False(x.Equals("String"));
+            Assert.True(nullvec == null);
+            Assert.True(null == nullvec);
+            Assert.False(nullvec == x);
+            Assert.False(x == nullvec);
+            Assert.False(nullvec != null);
+            Assert.False(null != nullvec);
+            Assert.True(nullvec != x);
+            Assert.True(x != nullvec);
+
+            Assert.False(x == null);
+            Assert.False(null == x);
+            Assert.True(x != null);
+            Assert.True(null != x);
+
+            Assert.True(x == y);
+            Assert.True(y == z);
+            Assert.True(z == x);
+            Assert.False(w == x);
+            Assert.False(w == y);
+            Assert.False(w == z);
+            Assert.True(w == w);
+
+            Assert.False(x != y);
+            Assert.False(y != z);
+            Assert.False(z != x);
+
+            Assert.True(w != x);
+            Assert.True(w != y);
+            Assert.True(w != z);
+            Assert.False(w != w);
+        }
+
     }
 }
